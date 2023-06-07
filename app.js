308aeec0660ae93cc11d785091fbf72ef086819f
XFAULTS <104481951+XFAULTS@users.noreply.github.com>
2023-06-08 01:45:16 +0530

    Update app.js 2nd try lets goo

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ let t = document.getElementById('T');
 window.addEventListener('scroll', ()=>{
     let value = window.scrollY;
     if(value > 1000 && value < 1300){
-        r.style.marginRight = (value-1000) * 2.0 + 'px';
+        r.style.marginRight = (value-1000) * 2.2 + 'px';
         r.style.marginTop = (value-1000) * 1.8 + 'px';
 
-        a.style.marginRight = (value-1000) * 1.1 + 'px';
+        a.style.marginRight = (value-1000) * 1.2 + 'px';
         a.style.marginTop = (value-1000) * -1.1 + 'px';
 
 
@@ -20,13 +20,13 @@ window.addEventListener('scroll', ()=>{
 
 
 
-        h.style.marginRight = (value-1000) * -0.4 + 'px';
+        h.style.marginRight = (value-1000) * -0.5 + 'px';
         h.style.marginTop = (value-1000) * 0.4 + 'px';
 
-        i.style.marginRight = (value-1000) * -1.1 + 'px';
+        i.style.marginRight = (value-1000) * -1.2 + 'px';
         i.style.marginTop = (value-1000) * -1.1 + 'px';
 
-        t.style.marginRight = (value-1000) * -2.0 + 'px';
+        t.style.marginRight = (value-1000) * -2.2 + 'px';
         t.style.marginTop = (value-1000) * 1.8 + 'px';
 
     }
@@ -38,11 +38,11 @@ window.addEventListener('scroll', ()=>{
     let pot = 300 * 1.8;
 
     if(value > 1300 && value < 1950){
-        r.style.marginTop = (por)+((value-1300) * 2.2) + 'px';
+        r.style.marginTop = (por)+((value-1300) * 2.4) + 'px';
         a.style.marginTop = (poa)+((value-1300) * 3.748) + 'px';
-        c.style.marginTop = (poc)+((value-1300) * 2.65) + 'px';
-        h.style.marginTop = (poh)+((value-1300) * 2.95) + 'px';
+        c.style.marginTop = (poc)+((value-1300) * 2.75) + 'px';
+        h.style.marginTop = (poh)+((value-1300) * 3.04) + 'px';
         i.style.marginTop = (poi)+((value-1300) * 3.748) + 'px';
-        t.style.marginTop = (pot)+((value-1300) * 2.2) + 'px';
+        t.style.marginTop = (pot)+((value-1300) * 2.4) + 'px';
     }
 })
