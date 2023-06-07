c7066e7cfdc128343cc0418fd8a7bdccc329be38
XFAULTS <104481951+XFAULTS@users.noreply.github.com>
2023-06-08 01:39:54 +0530

    Update app.js for phone

--- app.js
+++ app.js
@@ -7,35 +7,35 @@ let t = document.getElementById('T');
 
 window.addEventListener('scroll', ()=>{
     let value = window.scrollY;
-    if(value > 960 && value < 1300){
-        r.style.marginRight = (value-960) * 2.0 + 'px';
-        r.style.marginTop = (value-960) * 1.8 + 'px';
+    if(value > 1000 && value < 1300){
+        r.style.marginRight = (value-1000) * 2.0 + 'px';
+        r.style.marginTop = (value-1000) * 1.8 + 'px';
 
-        a.style.marginRight = (value-960) * 1.1 + 'px';
-        a.style.marginTop = (value-960) * -1.1 + 'px';
-
-
-        c.style.marginRight = (value-960) * 0.4 + 'px';
-        c.style.marginTop = (value-960) * 1 + 'px';
+        a.style.marginRight = (value-1000) * 1.1 + 'px';
+        a.style.marginTop = (value-1000) * -1.1 + 'px';
 
 
+        c.style.marginRight = (value-1000) * 0.4 + 'px';
+        c.style.marginTop = (value-1000) * 1 + 'px';
 
-        h.style.marginRight = (value-960) * -0.4 + 'px';
-        h.style.marginTop = (value-960) * 0.4 + 'px';
 
-        i.style.marginRight = (value-960) * -1.1 + 'px';
-        i.style.marginTop = (value-960) * -1.1 + 'px';
 
-        t.style.marginRight = (value-960) * -2.0 + 'px';
-        t.style.marginTop = (value-960) * 1.8 + 'px';
+        h.style.marginRight = (value-1000) * -0.4 + 'px';
+        h.style.marginTop = (value-1000) * 0.4 + 'px';
+
+        i.style.marginRight = (value-1000) * -1.1 + 'px';
+        i.style.marginTop = (value-1000) * -1.1 + 'px';
+
+        t.style.marginRight = (value-1000) * -2.0 + 'px';
+        t.style.marginTop = (value-1000) * 1.8 + 'px';
 
     }
-    let por = 340 * 1.8;
-    let poa = 340 * -1.1;
-    let poc = 340;
-    let poh = 340 * 0.4;
-    let poi = 340 * -1.1;
-    let pot = 340 * 1.8;
+    let por = 300 * 1.8;
+    let poa = 300 * -1.1;
+    let poc = 300;
+    let poh = 300 * 0.4;
+    let poi = 300 * -1.1;
+    let pot = 300 * 1.8;
 
     if(value > 1300 && value < 1950){
         r.style.marginTop = (por)+((value-1300) * 2.2) + 'px';
